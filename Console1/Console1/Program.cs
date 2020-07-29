1de41ed67f537b7f1a4a40bf1cef5332f762d2ec
andrew <khacmanle@gmail.com>
2020-07-29 22:25:45 +0800

    Fibonacci loop and tail recursive

--- Console1/Console1/Program.cs
+++ Console1/Console1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Console1.models;
+using Console1.service;
 using Point = System.Drawing.Point;
 
 namespace Console1
@@ -11,6 +12,9 @@ namespace Console1
             var point = new Point(3, 4);
             var d3Point = new Point3D(3, 4, 5);
             Console.WriteLine("Hello World!");
+            
+            Console.WriteLine(MyMath.FibLoop(16));
+            Console.WriteLine(MyMath.FibRecursive(16));
         }
     }
 }
--- Console1/Console1/service/MyMath.cs
+++ Console1/Console1/service/MyMath.cs
@@ -1,10 +1,46 @@
+using System;
+
 namespace Console1.service
 {
     public class MyMath
     {
-        public static int fib(int x)
+        public static int FibLoop(int x)
         {
-            return 0;
+            if (x < 2)
+            {
+                return x;
+            }
+            
+            var a = 0;
+            var b = 1;
+            
+            for (var i = 2; i < x; i++)
+            {
+                var temp = a + b;
+                a = b;
+                b = temp;
+            }
+            return a + b;
+        }
+
+        public static int FibRecursive(int x)
+        {
+            if (x < 2)
+            {
+                return x;
+            }
+            
+            return fibIter(x, 2, 0, 1);
+        }
+
+        private static int fibIter(int x, int i, int a, int b)
+        {
+            if (i == x)
+            {
+                return a + b;
+            }
+            
+            return fibIter(x, i + 1, b, a + b);
         }
     }
 }
